badbcbd4775a69c52cb86a9dbf190a16d6acff92
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-08-11 21:17:25 -0700

    Added some pieces for login

--- LoginAndReg/Controllers/HomeController.cs
+++ LoginAndReg/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LoginAndReg.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace LoginAndReg.Controllers
 {
@@ -18,10 +19,49 @@ namespace LoginAndReg.Controllers
             _logger = logger;
         }
 
+        public IActionResult Method(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                // Initializing a PasswordHasher object, providing our User class as its type
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                user.Password = Hasher.HashPassword(user, user.Password);
+                //Save your user object to the database
+                return RedirectToAction("Index");
+            }
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Login(LoginUser userSubmission)
+    {
+        if(ModelState.IsValid)
+        {
+            // If inital ModelState is valid, query for a user with provided email
+            var userInDb = dbContext.Users.FirstOrDefault(u => u.Email == userSubmission.Email);
+            // If no user exists with provided email
+            if(userInDb == null)
+            {
+                // Add an error to ModelState and return to View!
+                ModelState.AddModelError("Email", "Invalid Email/Password");
+                return View("SomeView");
+            }
+            
+            // Initialize hasher object
+            var hasher = new PasswordHasher<LoginUser>();
+            
+            // verify provided password against hash stored in db
+            var result = hasher.VerifyHashedPassword(userSubmission, userInDb.Password, userSubmission.Password);
+            
+            // result can be compared to 0 for failure
+            if(result == 0)
+            {
+                // handle failure (this should be similar to how "existing email" is handled)
+            }
+        }
+    }
 
         public IActionResult Privacy()
         {
---
+++ LoginAndReg/Models/LoginUser.cs
@@ -0,0 +1,8 @@
+namespace LoginAndReg.Models
+{
+    public class NewLogin
+    {
+    public string Email {get; set;}
+    public string Password { get; set; }
+    }
+}
